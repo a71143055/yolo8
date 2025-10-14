c7bc1b44c1a26de51a2831d64644667476b87672
a71143055 <a71143055@gmail.com>
2025-10-14 16:19:44 +0900

    yolo8

---
+++ fire.py
@@ -0,0 +1,11 @@
+import os
+from ultralytics import YOLO
+from multiprocessing import freeze_support
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+model = YOLO('yolov8n.pt')
+
+if __name__ == '__main__':
+    freeze_support()
+    model.predict(data='datasets/data.yaml',source='fire.jpg', save=True, show=True)
