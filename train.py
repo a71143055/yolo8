fd394c393cb6b758bef0927e8f9bf3f87c6524c7
a71143055 <a71143055@gmail.com>
2025-10-28 14:45:51 +0900

    yolo8

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from multiprocessing import freeze_support
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-model = YOLO('yolov8n-cls.pt')
+model = YOLO('yolov8m.pt')
 
 if __name__ == '__main__':
     freeze_support()
-    model.train(data='fashion-mnist',epoches=10, imgsz=32, device=0)
+    model.train(data='fashion-mnist', epoch=10, imgsz=320, device=0)
